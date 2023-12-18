669fa80e465a9e6716534db50c757a3dc44484e0
Martin <mr6058@gmail.com>
2023-12-18 23:00:35 +0100

    Finished day6

---
+++ src/Day6/BoatRace.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day6
+{
+    public static class BoatRace
+    {
+        private record RaceRecord(long Time, long Distance);
+
+        private static async Task<List<RaceRecord>> ReadRaceRecordsFromFile()
+        {
+            var almanacLines = await File.ReadAllLinesAsync("./Day6/RaceRecords.txt");
+            var raceRecords = new List<RaceRecord>();
+
+            var times = GetNumbersFromStringArr(almanacLines[0].Split(" "));
+            var distances = GetNumbersFromStringArr(almanacLines[1].Split(" "));
+
+            for (int i = 0; i < times.Length; i++)
+                raceRecords.Add(new RaceRecord(times[i], distances[i]));
+
+            return raceRecords;
+        }
+
+        private static long[] GetNumbersFromStringArr(string[] strings)
+        {
+            var numbers = new List<long>();
+
+            foreach (var str in strings)
+                if (long.TryParse(str, out long number))
+                    numbers.Add(number);
+
+            return numbers.ToArray();
+        }
+
+        private static long CalculateDistance(long timeHeld, long timeLimit)
+        {
+            var timeRemaining = timeLimit - timeHeld;
+
+            var distanceMoved = timeHeld * timeRemaining;
+
+            return distanceMoved;
+        }
+
+        private static long GetCountOfWaysToWin(RaceRecord raceRecord)
+        {
+            var winCount = 0;
+            var timeHeld = 1;
+
+            while (timeHeld < raceRecord.Time)
+            {
+                var distanceMoved = CalculateDistance(timeHeld, raceRecord.Time);
+
+                if (distanceMoved > raceRecord.Distance)
+                    winCount++;
+
+                timeHeld++;
+            }
+
+            return winCount;
+        }
+
+        private static long GetMultiplicationOfWins(List<RaceRecord> raceRecords)
+        {
+            long sum = 1;
+            foreach (var raceRecord in raceRecords)
+            {
+                sum *= GetCountOfWaysToWin(raceRecord);
+            }
+
+            return sum;
+        }
+
+        public static async Task<long> GetPowerOfWaysToWin()
+        {
+            var records = await ReadRaceRecordsFromFile();
+            var power = GetMultiplicationOfWins(records);
+            return power;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using AdventOfCode2023.Day2;
 using AdventOfCode2023.Day3;
 using AdventOfCode2023.Day4;
 using AdventOfCode2023.Day5;
+using AdventOfCode2023.Day6;
 using AdventOfCode2023.Day7;
 using AdventOfCode2023.Day8;
 
@@ -41,6 +42,9 @@ namespace AdventOfCode2023
                     break;
                 case 5:
                     Console.WriteLine(await FoodProduction.GetLowestLocationNumber());
+                    break;
+                case 6:
+                    Console.WriteLine(await BoatRace.GetPowerOfWaysToWin());
                     break;
                 case 7:
                     Console.WriteLine(await CamelCards.GetTotalWinnings());
