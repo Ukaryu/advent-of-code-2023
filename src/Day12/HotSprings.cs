7e4aa7096535102bc4a5c307ba48d1f1c20d2dd2
Martin <mr6058@gmail.com>
2023-12-19 22:32:25 +0100

    Nonfunctional part2

--- src/Day12/HotSprings.cs
+++ src/Day12/HotSprings.cs
@@ -1,8 +1,12 @@
-﻿namespace AdventOfCode2023
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Win32.SafeHandles;
+
+namespace AdventOfCode2023
 {
     public static class HotSprings
     {
-        private record ConditionRecord(string Arrangement, List<int> GroupSizes);
+        private record ConditionRecord(string Arrangement, int[] GroupSizes);
 
         private static async Task<IEnumerable<ConditionRecord>> ReadConditionRecords()
         {
@@ -15,31 +19,50 @@
                 var arrangmentAndGroupSize = conditionRecordLine.Split(' ').ToList();
 
                 var arrangement = arrangmentAndGroupSize[0];
+
+                var unfoldedArrangement = arrangement;
+                for (int i = 0; i < 4; i++)
+                    unfoldedArrangement += '?' + arrangement;
+
                 var groupSizes = arrangmentAndGroupSize[1].Split(',').Select(int.Parse).ToList();
 
-                conditionRecords.Add(new(arrangement, groupSizes));
+                var unfoldedGroupSizes = new List<int>();
+                for (int i = 0; i < 5; i++)
+                    unfoldedGroupSizes.AddRange(groupSizes);
+
+                conditionRecords.Add(new(unfoldedArrangement, [.. unfoldedGroupSizes]));
             }
 
             return conditionRecords;
         }
 
-        private static int GetAllPossibleArrangements(IEnumerable<ConditionRecord> records)
+        private static long GetAllPossibleArrangements(IEnumerable<ConditionRecord> records)
         {
-            var sumPossibleArrangements = 0;
+            long sumPossibleArrangements = 0;
             foreach (var record in records)
             {
                 sumPossibleArrangements += GetPossibleArrangmentsCount(
                     record.Arrangement,
-                    record.GroupSizes.ToArray()
+                    record.GroupSizes
                 );
             }
 
             return sumPossibleArrangements;
         }
 
-        private static int GetPossibleArrangmentsCount(string arrangement, int[] sizes)
+        //private static Dictionary<string, long> EvaluatedArrangements =
+        //    new Dictionary<string, long>();
+        //private static Func<string, int[], long> CachedFunc = Memoized.Of<string, int[], long>(
+        //    GetPossibleArrangmentsCount,
+        //    new MyEqualityComparer()
+        //);
+
+        static Dictionary<(string, int[]), long> EvaluatedArrangements =
+            new(new MyEqualityComparer());
+
+        private static long GetPossibleArrangmentsCount(string arrangement, int[] sizes)
         {
-            var arrangementsCount = 0;
+            long arrangementsCount = 0;
 
             if (arrangement == "")
                 if (sizes.Length == 0)
@@ -53,6 +76,9 @@
                 else
                     return 1;
 
+            if (EvaluatedArrangements.TryGetValue((arrangement, sizes), out long cachedCount))
+                return cachedCount;
+
             if (".?".Contains(arrangement[0]))
                 arrangementsCount += GetPossibleArrangmentsCount(arrangement[1..], sizes);
 
@@ -62,27 +88,83 @@
                     && !arrangement[..sizes[0]].Contains('.')
                     && (sizes[0] == arrangement.Length || arrangement[sizes[0]] != '#')
                 )
-                    if (sizes[0] + 1 <= arrangement.Length)
-                        arrangementsCount += GetPossibleArrangmentsCount(
-                            arrangement[(sizes[0] + 1)..],
-                            sizes[1..]
-                        );
-                    else
-                        arrangementsCount += GetPossibleArrangmentsCount(
-                            arrangement[sizes[0]..],
-                            sizes[1..]
-                        );
+                {
+                    var arrStart = sizes[0] + 1 <= arrangement.Length ? sizes[0] + 1 : sizes[0];
+                    arrangementsCount += GetPossibleArrangmentsCount(
+                        arrangement[arrStart..],
+                        sizes[1..]
+                    );
+                }
                 else
                     arrangementsCount += 0;
+
+            EvaluatedArrangements.Add((arrangement, sizes), arrangementsCount);
 
             return arrangementsCount;
         }
 
-        public static async Task<int> GetSumOfAmountOfPossibleArrangements()
+        public static async Task<long> GetSumOfAmountOfPossibleArrangements()
         {
             var records = await ReadConditionRecords();
             var possibleArrangements = GetAllPossibleArrangements(records);
             return possibleArrangements;
+        }
+    }
+
+    public class Memoized
+    {
+        public static Func<T1, T2, TRet> Of<T1, T2, TRet>(Func<T1, T2, TRet> f)
+        {
+            ConcurrentDictionary<(T1, T2), TRet> cache = new ConcurrentDictionary<(T1, T2), TRet>();
+
+            return (arg1, arg2) =>
+                cache.GetOrAdd((arg1, arg2), (xarg) => f(xarg.Item1, xarg.Item2));
+        }
+
+        public static Func<T1, T2, TRet> Of<T1, T2, TRet>(
+            Func<T1, T2, TRet> f,
+            IEqualityComparer<(T1, T2)> equalityComparer
+        )
+        {
+            ConcurrentDictionary<(T1, T2), TRet> cache = new ConcurrentDictionary<(T1, T2), TRet>(
+                equalityComparer
+            );
+
+            return (arg1, arg2) =>
+                cache.GetOrAdd((arg1, arg2), (xarg) => f(xarg.Item1, xarg.Item2));
+        }
+    }
+
+    public class MyEqualityComparer : IEqualityComparer<(string, int[])>
+    {
+        public bool Equals((string, int[]) x, (string, int[]) y)
+        {
+            if (x.Item2.Length != y.Item2.Length)
+                return false;
+
+            for (int i = 0; i < x.Item2.Length; i++)
+                if (x.Item2[i] != y.Item2[i])
+                    return false;
+
+            if (!x.Item1.Equals(x.Item1))
+                return false;
+
+            return true;
+        }
+
+        public int GetHashCode([DisallowNull] (string, int[]) obj)
+        {
+            int result = 17;
+            for (int i = 0; i < obj.Item2.Length; i++)
+            {
+                unchecked
+                {
+                    result = result * 23 + obj.Item2[i].GetHashCode();
+                }
+            }
+            result += obj.Item1.Select(c => c.GetHashCode()).Sum();
+
+            return result;
         }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -6,60 +6,64 @@ namespace AdventOfCode2023
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Choose day:");
-            var day = Console.ReadLine();
-            int parsedDay = 0;
-
-            while (!int.TryParse(day, out parsedDay))
+            var day = "";
+            while (day != "exit")
             {
-                Console.WriteLine("Input a integer!");
-                day = Console.ReadLine();
-                Console.Clear();
                 Console.WriteLine("Choose day:");
+                day = Console.ReadLine();
                 Console.WriteLine(day);
-            }
+                Console.Clear();
 
-            switch (parsedDay)
-            {
-                case 1:
-                    Console.WriteLine(await Trebuchet.SumCalibrationValues());
-                    break;
-                case 2:
-                    Console.WriteLine(await CubeConundrum.GetSumOfValidGameIds());
-                    break;
-                case 3:
-                    Console.WriteLine(await GearRatios.GetSumOfAllPartNumbers());
-                    break;
-                case 4:
-                    Console.WriteLine(await Scratchcards.GetSumOfWinningScratchCards());
-                    break;
-                case 5:
-                    Console.WriteLine(await FoodProduction.GetLowestLocationNumber());
-                    break;
-                case 6:
-                    Console.WriteLine(await BoatRace.GetPowerOfWaysToWin());
-                    break;
-                case 7:
-                    Console.WriteLine(await CamelCards.GetTotalWinnings());
-                    break;
-                case 8:
-                    Console.WriteLine(await HauntedWasteland.GetRequiredSteps());
-                    break;
-                case 9:
-                    Console.WriteLine(await MirageMaintenance.GetSumOfExtrapolatedValues());
-                    break;
-                case 10:
-                    Console.WriteLine(await PipeMaze.GetStepsFarthestFromStarting());
-                    break;
-                case 11:
-                    Console.WriteLine(await CosmicExpansion.GetSumOfShortestPathsBetweenGalaxies());
-                    break;
-                case 12:
-                    Console.WriteLine(await HotSprings.GetSumOfAmountOfPossibleArrangements());
-                    break;
-                default:
-                    Console.WriteLine("No valid day chosen");
-                    break;
+                if (int.TryParse(day, out int parsedDay))
+                {
+                    Console.WriteLine("Output:");
+                    switch (parsedDay)
+                    {
+                        case 1:
+                            Console.WriteLine(await Trebuchet.SumCalibrationValues());
+                            break;
+                        case 2:
+                            Console.WriteLine(await CubeConundrum.GetSumOfValidGameIds());
+                            break;
+                        case 3:
+                            Console.WriteLine(await GearRatios.GetSumOfAllPartNumbers());
+                            break;
+                        case 4:
+                            Console.WriteLine(await Scratchcards.GetSumOfWinningScratchCards());
+                            break;
+                        case 5:
+                            Console.WriteLine(await FoodProduction.GetLowestLocationNumber());
+                            break;
+                        case 6:
+                            Console.WriteLine(await BoatRace.GetPowerOfWaysToWin());
+                            break;
+                        case 7:
+                            Console.WriteLine(await CamelCards.GetTotalWinnings());
+                            break;
+                        case 8:
+                            Console.WriteLine(await HauntedWasteland.GetRequiredSteps());
+                            break;
+                        case 9:
+                            Console.WriteLine(await MirageMaintenance.GetSumOfExtrapolatedValues());
+                            break;
+                        case 10:
+                            Console.WriteLine(await PipeMaze.GetStepsFarthestFromStarting());
+                            break;
+                        case 11:
+                            Console.WriteLine(
+                                await CosmicExpansion.GetSumOfShortestPathsBetweenGalaxies()
+                            );
+                            break;
+                        case 12:
+                            Console.WriteLine(
+                                await HotSprings.GetSumOfAmountOfPossibleArrangements()
+                            );
+                            break;
+                        default:
+                            Console.WriteLine("No valid day chosen");
+                            break;
+                    }
+                }
             }
         }
     }
